b358c9712ab88f1d409da2731843559d44ee7113
Mohammad Alali <alalim4@my.erau.edu>
2019-04-07 03:02:13 -0400

    Renaming

--- rdf2walk/MainEntry.java
+++ rdf2walk/MainEntry.java
@@ -7,7 +7,7 @@ import org.apache.jena.query.Dataset;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-public class Main
+public class MainEntry
 {
 	public static void main(String[] args)
 	{
